0ffe745680360b69824a7367effd24251445d059
thanhvuongsk <thanhvuongsk@gmail.com>
2018-08-21 12:10:58 +0700

    Bai Hom Nay + Tra No reponsive

---
+++ 2018-8-21/baitap.js
@@ -0,0 +1,10 @@
+function evnetbuton(){
+    var x = document.getElementById("bien");
+    
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+    
+}
