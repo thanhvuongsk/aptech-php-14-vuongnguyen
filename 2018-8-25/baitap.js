2f77a4c8b15002c494c9d48eabb354d318ba8c1b
thanhvuongsk <thanhvuongsk@gmail.com>
2018-08-25 12:24:42 +0700

    Bai Tap

---
+++ 2018-8-25/baitap.js
@@ -0,0 +1,60 @@
+function evnetbuton(){
+    // var x = document.getElementById("bien");
+    
+    //  if (x.style.display === "none") {
+    //      x.style.display = "block";
+    //  } else {
+    //      x.style.display = "none";
+    //  }
+
+    var x = document.getElementById("bien") ;
+    x.className;
+   // x.classList;
+    
+    console.log( x.className);
+    // if(x.className.includes('d-block')){ classname chứa chỉ một thuộc tính của class
+      
+    //   x.className = 'd-none';
+     
+    // }
+    // else{
+    //     x.className = 'd-block';
+    //     x.className += ' bg-dark'; 
+       
+        
+    // }
+    if(x.classList.contains('d-none')){ //list chứa luôn một mãng của classs
+        x.classList.remove('d-none');
+        x.classList.add('d-block');
+    }
+    else{
+        x.classList.remove('d-block');
+        x.classList.add('d-none');
+    }
+
+
+}function demohamper(){
+    //flex-column
+    var n = document.getElementById('hamberger');
+    n.classList;
+    if(x.classList.contains('d-none')){ //list chứa luôn một mãng của classs
+        x.classList.remove('d-none');
+        x.classList.add('d-block');
+    }
+    else{
+        x.classList.remove('d-block');
+        x.classList.add('d-none');
+    }
+    
+}
+
+$(document).ready(function(){
+    $('#an').click(function(){
+         $('#hiden').toggle(1000);
+    });
+
+    
+
+}
+
+);
